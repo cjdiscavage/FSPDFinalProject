03ad670ed604313ef0916991e76dacdb5107bf0e
CJ Discavage <cjdisc@truevine.net>
2022-03-24 11:40:34 -0500

    Filled pages with content.  Started creating the reservation page

---
+++ FSPDFinalProject/Controllers/ReservationController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FSPDFinalProject.Data.EF;
+
+namespace FSPDFinalProject.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private ReservationSystemEntities db = new ReservationSystemEntities();
+
+        // GET: Reservation
+        public ActionResult Index()
+        {
+            var reservations = db.Reservations;
+            return View(reservations.ToList());
+        }
+
+        // GET: Reservation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reservation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
